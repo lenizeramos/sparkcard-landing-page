f0415f667f2fa4c76a2a454b322353d315a2e5da
Lenize Ramos <lenizemr@gmail.com>
2025-02-06 11:25:21 -0800

    add styles to email sectionr

--- my-app/src/app/components/EmailSection.tsx
+++ my-app/src/app/components/EmailSection.tsx
@@ -14,23 +14,25 @@ const EmailSection: React.FC = () => {
       id="email"
       className="h-[75vh] w-full flex-1 flex flex-col justify-center items-center bg-gray-300"
     >
-      <h2 className="text-4xl font-bold font-mono text-center mb-6">
-        {sectionContent.h2}
-      </h2>
-      <p className="mb-4">{sectionContent.text}</p>
-      <div className="flex flex-row gap-2 mb-4 w-[60%]">
-        <input
-          type="email"
-          name="emailInput"
-          id="emailInput"
-          placeholder={sectionContent.placeholder}
-          className="text-left pl-3 focus:outline-none focus:ring-2 focus:ring-gray-900 w-[65%]"
-        />
-        <div className="w-[35%]">
-          <Button buttonLabel={sectionContent.buttonLabel} />
+      <div className="w-[90%] flex flex-col ustify-center items-center">
+        <h2 className="text-4xl font-bold font-mono text-center mb-6">
+          {sectionContent.h2}
+        </h2>
+        <p className="mb-4">{sectionContent.text}</p>
+        <div className="flex flex-row gap-2 mb-4 sm:w-[60%] md:w-[70%] lg:w-[30%]">
+          <input
+            type="email"
+            name="emailInput"
+            id="emailInput"
+            placeholder={sectionContent.placeholder}
+            className="text-left pl-3 focus:outline-none focus:ring-2 focus:ring-gray-900 w-[65%]"
+          />
+          <div className="w-[35%]">
+            <Button buttonLabel={sectionContent.buttonLabel} />
+          </div>
         </div>
+        <span className="text-sm">{sectionContent.span}</span>
       </div>
-      <span className="text-sm">{sectionContent.span}</span>
     </section>
   );
 };
